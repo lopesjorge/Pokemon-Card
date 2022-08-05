aeec0ce67507e88ae7896b4d2c864da50efc6b60
jorge <jorgelopeslopes@outlook.com>
2022-08-05 14:40:50 -0300

    Up - linkImage com sem funcionar

--- app.js
+++ app.js
@@ -9,13 +9,22 @@ const fecthPokemons = () => {
 
     Promise.all(pokemonPromise)
     .then(pokemons => {
-       /// console.log(pokemons)
-       const liPokemons = pokemons.reduce((accumulator, pokemon) =>{
-             accumulator+= `<li>${pokemon.name}</li>`
+
+        const liPokemons = pokemons.reduce((accumulator, pokemon) =>{
+
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+             accumulator+= 
+            `<li class="card">
+             <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://translate.google.com/website?sl=en&tl=pt&hl=pt-BR&prev=search&u=https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg"/>
+             <h2 class="card-title"> ${pokemon.id}. ${pokemon.name}</h5>
+             <p class="card-subtitle"> ${types.join(' | ')}</p>
+             </li>`
              return accumulator;
        }, '')
 
-       console.log(liPokemons)
+       const ul = document.querySelector('[data-js = "pokedex"]')
+       ul.innerHTML = liPokemons
     })
 }
 
